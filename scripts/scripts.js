897c5ebcaf6bf18f0020dd4aa9f8cd8cacedeabc
Syb Wartna <sybren.wartna@cognizant.com>
2023-10-23 15:44:04 +0200

    remove redundant function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,28 +39,6 @@ export function getTextLabel(key) {
   return placeholders.data.find((el) => el.Key === key)?.Text || key;
 }
 
-/**
- * Create an element with the given id and classes.
- * @param {string} tagName the tag
- * @param {string[]|string} classes the class or classes to add
- * @param {object} props any other attributes to add to the element
- * @returns the element
- */
-export function createElement(tagName, classes, props) {
-  const elem = document.createElement(tagName);
-  if (classes) {
-    const classesArr = (typeof classes === 'string') ? [classes] : classes;
-    elem.classList.add(...classesArr);
-  }
-  if (props) {
-    Object.keys(props).forEach((propName) => {
-      elem.setAttribute(propName, props[propName]);
-    });
-  }
-
-  return elem;
-}
-
 function getCTAContainer(ctaLink) {
   return ['strong', 'em'].includes(ctaLink.parentElement.localName)
     ? ctaLink.parentElement.parentElement
